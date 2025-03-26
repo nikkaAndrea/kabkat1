9fad10a4b722ddd496419860fc33bcdb9e74a846
nikkaAndrea <sagcalnikkaandrea@gmail.com>
2025-03-26 15:14:03 +0800

    Create Dashboard.vue

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+      <div class="title-page">
+        <Welcome />
+      </div>
+
+      <script>
+import Welcome from "@/components/Welcome.vue";
+
+export default {
+  name: "Dashboard",
+  components: {
+    Welcome,
+  },
+  data() {
+    return {
+    }
+}
+}
+</script>
+</div>
+</template>
+
+<style scoped>
+.container {
+  margin-left: 26%;
+  margin-top: 85px;
+  height: 100%;
+}
+</style>
