ff012f9c17d9f3a04d37c0b2b38419e2ac15110f
oni <197040981+nikandree@users.noreply.github.com>
2025-05-18 10:46:54 +0800

    text

---
+++ src/components/Title.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>One Matain, Galing ng Batang Matain!</h1>
+  </template>
+  
+  <script>
+  export default {
+    name: "Title",
+  };
+  </script>
+  
+  <style scoped>
+  h1 {
+    color: #000;
+    font-size: 40px;
+    padding-top: 30px;
+    padding-left: 15px;
+  }
+  </style>
+  
